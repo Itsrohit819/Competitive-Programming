31af82f048dad52f90f5c3773a2807d1bf39bed7
Rohit <singhrohitckt819@gmail.com>
2024-02-29 09:05:51 +0530

    CP

---
+++ Mahmoud_and_Ehab_and_The_Even_Odd_game.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	long long n;
+	cin >> n;
+	if(n%2==0){
+		cout << "Mahmoud" << endl;
+	}
+	if(n%2==1){
+		cout << "Ehab" << endl;
+	}
+}
