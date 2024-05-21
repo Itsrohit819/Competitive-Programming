4db184f89197fd3f5a8f6c3cbe6f604b9ff2ad0d
Rohit <singhrohitckt819@gmail.com>
2024-05-21 20:21:36 +0530

    first commit

---
+++ Bad Boy.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int m;
+	cin >> m;
+	while(m--){
+		int a,b,c,d;
+		cin >> a >> b >> c >> d;
+		cout << "1 " << b << " " << a << " 1" << endl;
+	}
+}
