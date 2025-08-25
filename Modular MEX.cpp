03911bb04cc7c774937ee3e7763b4a4ba7ee16a4
Rohit <singhrohitckt819@gmail.com>
2025-08-25 09:39:05 +0530

    CP

---
+++ Modular MEX.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int solve(){
+	int n;
+	cin >> n;
+	cout << (n+1)/2 << endl;
+	return 0;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1; 
+	cin>>t;
+    while(t--) solve();
+}
