a81adb2005046f81385338418ade608d9751e230
Rohit <singhrohitckt819@gmail.com>
2025-08-31 22:52:17 +0530

    cp

---
+++ simple_snake_seq.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            if(j%2==0){
+                cout << (5*j-i+1) << " ";
+            }
+            else{
+                cout << (5*(j-1)+i) << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
